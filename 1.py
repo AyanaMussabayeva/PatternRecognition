98e4a7951df4a426130a07e83c94c3d1b80917a9
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2018-01-10 12:40:06 +0600

    task 1

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
+t = int(input())
+
 a=input().split()
 for i in range(len(a)):
 	a[i]=int(a[i])
-t = int(input())
+
 a.sort()
 b = list(reversed(a))
-print(b)
 sum = 0
 if len(b) < t:
 	for i in range(len(b)): 
